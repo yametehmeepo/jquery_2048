7ba5a750137c6ebf68004f25124b75985157e577
“LXP” <yameteh_meepo@163.com>
2018-11-13 11:51:43 +0800

    初始化2048棋盘

---
+++ js/main.js
@@ -0,0 +1,54 @@
+$(function(){
+	newgame()
+})
+
+
+function newgame(){
+	init()
+}
+
+function init(){
+	for(var i = 0; i < 4; i++){
+		for(var j = 0; j < 4; j++){
+			var v = getLeftTop(i, j)
+			$('#grid-item-' + i + '-' + j).css({'left': v.left, 'top': v.top})
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
