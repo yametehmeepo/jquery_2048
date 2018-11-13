7f196bf47bfb5d3ccecf45058b2f2a70df47fe38
“LXP” <yameteh_meepo@163.com>
2018-11-13 14:30:52 +0800

    初始化两个数字

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+var board = [];
+var score = 0;
+
 $(function(){
 	newgame()
 })
@@ -14,9 +17,64 @@ function init(){
 			$('#grid-item-' + i + '-' + j).css({'left': v.left, 'top': v.top})
 		}
 	}
+	board = [
+		[0, 0, 0, 0],
+		[0, 0, 0, 0],
+		[0, 0, 0, 0],
+		[0, 0, 0, 0]
+	]
+	initNumberCell()
+	var randomX = parseInt(Math.floor(Math.random() * 4))
+	var randomY = parseInt(Math.floor(Math.random() * 4))
+	var number = Math.random() < 0.5?2:4
+	board[randomX][randomY] = number
+	randomX = parseInt(Math.floor(Math.random() * 4))
+	randomY = parseInt(Math.floor(Math.random() * 4))
+	while(true){
+		if(board[randomX][randomY] == 0){
+			break
+		}else{
+			randomX = parseInt(Math.floor(Math.random() * 4))
+			randomY = parseInt(Math.floor(Math.random() * 4))
+		}
+	}
+	number = Math.random() < 0.5?2:4
+	board[randomX][randomY] = number
+	//console.log(board)
+	for(var i = 0; i < 4; i++){
+		for(var j = 0; j < 4; j++){
+			if(board[i][j] != 0){
+				var numbercell = $('#number-cell-' + i + '-' + j)
+				numbercell.css({
+					'width': '100px',
+					'height': '100px',
+					'left': getLeftTop(i, j).left,
+					'top': getLeftTop(i, j).top,
+					'background-color': getBackgroundColor(board[i][j]),
+					'color': getColor(board[i][j])
+				})
+				numbercell.text(board[i][j])
+			}
+		}
+	}
+
 }
 
-
+function initNumberCell(){
+	$('.numbercell').remove()
+	for(var i = 0; i < 4; i++){
+		for(var j = 0; j < 4; j++){
+			var v = getLeftTop(i, j)
+			$('.container').append($('<div class="numbercell" id="number-cell-' + i + '-' + j +'"></div>'))
+			$('#number-cell-' + i + '-' + j).css({
+				width: 0,
+				height: 0,
+				left: v.left + 50,
+				top: v.top + 50
+			})
+		}
+	}
+}
 
 
 
--- js/support.js
+++ js/support.js
@@ -3,9 +3,56 @@ function getLeftTop(i, j){
 	return {left: 20 + 120 * j, top: 20 + 120 * i }
 }
 
+function getBackgroundColor(number){
+	switch (number){
+		case 2: 
+			return '#eee4da';
+			break;
+		case 4: 
+			return '#ede0c8';
+			break;
+		case 8: 
+			return '#f2b179';
+			break;
+		case 16: 
+			return '#f59563';
+			break;
+		case 32: 
+			return '#f67c5f';
+			break;
+		case 64: 
+			return '#f65e3b';
+			break;
+		case 128: 
+			return '#edcf72';
+			break;
+		case 256: 
+			return '#edcc61';
+			break;
+		case 512: 
+			return '#9c0';
+			break;
+		case 1024: 
+			return '#33b5e5';
+			break;
+		case 2048: 
+			return '#09c';
+			break;
+		case 4096: 
+			return '#a6c';
+			break;
+		case 8192: 
+			return '#93c';
+			break;
+	}
+}
 
-
-
+function getColor(number){
+	if(number <= 4){
+		return '#776e65'
+	}
+	return '#fff'
+}
 
 
 
